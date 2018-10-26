ec917bf7281e2613f6a7f00bb1e44e97b3d00d31
tam <tamara.acevedoavila@gmail.com>
2018-10-26 13:44:52 -0300

    cambio app.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module BitApp
   class Application < Rails::Application
 
     config.assets.initialize_on_precompile = false
-    config.assets.js_compressor = Uglifier.new(harmony: true, compress: { reduce_vars: false, collapse_vars: false })
+    config.assets.js_compressor = NoCompression.new
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += %W(#{config.root}/lib/api)
